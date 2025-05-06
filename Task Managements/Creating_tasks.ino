9e72dbc0edcaa9b8313b70acd1f3f0f0833c5869
himanshugithu <fanibharehimanshu@gmail.com>
2025-05-06 12:34:39 +0530

    add readme for the task managment

---
+++ Task Managements/Creating_tasks.ino
@@ -0,0 +1,65 @@
+
+#define RED 2
+#define YELLOW 4
+#define BLUE 15
+
+typedef int TaskProfiler;
+
+TaskProfiler RedLEDProfiler;
+TaskProfiler YellowLEDProfiler;
+TaskProfiler BlueLEDProfiler;
+
+void setup()
+{
+  Serial.begin(9600);
+  xTaskCreate(redLedControllerTask,
+              "Red LED Task",
+              1024,
+              NULL,
+              1,
+              NULL);
+  xTaskCreate(blueLedControllerTask,
+              "Blue LED Task",
+              1024,
+              NULL,
+              1,
+              NULL);
+  xTaskCreate(yellowLedControllerTask,
+              "Yellow LED Task",
+              1024,
+              NULL,
+              1,
+              NULL);
+}
+
+void redLedControllerTask(void *pvParameters)
+{
+  pinMode(RED, OUTPUT);
+
+  while (1)
+  {
+    Serial.println("This is RED");
+  }
+}
+
+void blueLedControllerTask(void *pvParameters)
+{
+  pinMode(BLUE, OUTPUT);
+  while (1)
+  {
+    Serial.print("This is BLUE");
+  }
+}
+
+void yellowLedControllerTask(void *pvParameters)
+{
+  pinMode(YELLOW, OUTPUT);
+  while (1)
+  {
+    Serial.println("This is YELLOW");
+  }
+}
+
+void loop()
+{
+}
