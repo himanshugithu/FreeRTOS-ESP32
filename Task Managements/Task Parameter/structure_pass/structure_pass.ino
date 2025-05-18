f1246bdd8a7e90ff2fe34b6b13efb94c7e31e069
Himanshu Fanibhare <114344512+himanshugithu@users.noreply.github.com>
2025-05-18 20:50:39 +0530

    Update structure_pass.ino

--- Task Managements/Task Parameter/structure_pass/structure_pass.ino
+++ Task Managements/Task Parameter/structure_pass/structure_pass.ino
@@ -1,32 +1,29 @@
+struct basicLedConfig
+{
+  uint16_t pin;
+  uint16_t delayMS;
+};
 
-// Structure for task parameters
-typedef struct {
-    int pin;
-    int delayMS;
-} LedConfig;
+basicLedConfig redConfig = {2,100};
+basicLedConfig blueConfig = {4,100};
 
-// Creating two different LED configurations
-LedConfig redConfig = {6, 500};   // Pin 6, 500ms delay
-LedConfig blueConfig = {7, 300};  // Pin 7, 300ms delay
-
-void setup() {
-    Serial.begin(9600);
-    // Creating tasks with structured parameters
-    xTaskCreate(ledTask, "Red LED", 100, &redConfig, 1, NULL);
-    xTaskCreate(ledTask, "Blue LED", 100, &blueConfig, 1, NULL);
+void setup()
+{
+  Serial.begin(115200);
+  xTaskCreate(ledController,"led Control",1024,&redConfig,1,NULL);
+  xTaskCreate(ledController,"led control",1024,&blueConfig,1,NULL);
 }
 
-void ledTask(void *pvParameters) {
-    // Accessing the passed structure
-    LedConfig *config = (LedConfig *)pvParameters;
-    pinMode(config->pin, OUTPUT);
-    
-    while (1) {
-        digitalWrite(config->pin, HIGH);
-        vTaskDelay(config->delayMS / portTICK_PERIOD_MS);
-        digitalWrite(config->pin, LOW);
-        vTaskDelay(config->delayMS / portTICK_PERIOD_MS);
-    }
+void ledController(void* pv)
+{
+  basicLedConfig* config = (basicLedConfig*)pv;
+  pinMode(config->pin,OUTPUT);
+  while(1)
+  {
+    // digitalWrite(config->pin,!digitalRead(config->pin));
+    Serial.println(config->pin);
+    vTaskDelay(config->delayMS / portTICK_PERIOD_MS);
+  }
 }
 
 void loop() {}
